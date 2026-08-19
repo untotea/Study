1c00e6f97693409518bd4c9362ac17722d8001db
untotea <platonov1711@gmail.com>
2026-08-19 17:01:48 +0300

    Комментирование ненужных принтов

--- ML/scikit-learn/iris_sickit_learn.py
+++ ML/scikit-learn/iris_sickit_learn.py
@@ -4,14 +4,14 @@ import numpy as np
 iris = datasets.load_iris() #Загружаем данные ириса
 X = iris.data[:, [2, 3]] #берем длину и ширину лепестка
 y = iris.target #Берем метку класса. target в scikit-learn по умолчанию имеет ввиду метки классов.
-print('Метки классов:', np.unique(y))
+#print('Метки классов:', np.unique(y))
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1, stratify=y) #Случайным образом разбиваем данные на 30% ипсытательных и 70% обучающих.
 
-print('Количества меток в y:', np.bincount(y))
-print('Количества меток в y_train:', np.bincount(y_train))
-print('Количества меток в y_test:', np.bincount(y_test))
+#print('Количества меток в y:', np.bincount(y))
+#print('Количества меток в y_train:', np.bincount(y_train))
+#print('Количества меток в y_test:', np.bincount(y_test))
 
 #Стандартизация признаков
 from sklearn.preprocessing import StandardScaler
@@ -27,12 +27,12 @@ ppn = Perceptron(eta0=0.1, random_state=1)
 ppn.fit(X_train_std, y_train) 
 
 y_pred = ppn.predict(X_test_std)
-print('Неправильно классифицированных образцов: %d' % (y_test != y_pred).sum())
+#print('Неправильно классифицированных образцов: %d' % (y_test != y_pred).sum())
 
 #Метрики эффективности
 from sklearn.metrics import accuracy_score
 
-print('Правильность: %.3f' % accuracy_score(y_test, y_pred))
-print('Правильность: %.3f' % ppn.score(X_test_std, y_test))
+#print('Правильность: %.3f' % accuracy_score(y_test, y_pred))
+#print('Правильность: %.3f' % ppn.score(X_test_std, y_test))
 
 
